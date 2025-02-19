f870ede4a4d9982d4d2fc40e5a617a7f6a19ef0a
Yann Le Vagueres <yann.lv@live.fr>
2025-02-20 00:38:40 +0100

    Added bases of Jellyfin and Graph API

---
+++ graph_api.py
@@ -0,0 +1,132 @@
+import requests
+from dotenv import load_dotenv
+import copy
+
+import os
+
+load_dotenv()
+
+# This is API key for facebook messenger.
+API = "https://graph.facebook.com/v22.0/me/messages"
+
+
+class GraphAPI:
+    """Real docs at https://developers.facebook.com/docs/messenger-platform/reference/send-api/"""
+
+    page_access_token: str
+
+    def __init__(self, access_token: str):
+        self.page_access_token = access_token
+
+    @property
+    def params(self):
+        return {"access_token": self.page_access_token}
+
+    @property
+    def headers(self):
+        return {"Content-Type": "application/json"}
+
+    def create_postback_button(self, title: str, payload: str):
+        return {"type": "postback", "title": title, "payload": str(payload)}
+
+    def create_url_button(self, title: str, url: str, webview_height_ratio=None):
+        url_button = {"type": "web_url", "url": url, "title": title}
+        if webview_height_ratio is not None:
+            url_button["webview_height_ratio"] = webview_height_ratio
+        return url_button
+
+    def create_default_action(self, button):
+        action = copy.deepcopy(button)
+        if "title" in action:
+            del action["title"]
+        return action
+
+    def create_single_generic(
+        self,
+        title: str,
+        subtitle=None,
+        image_url=None,
+        default_action=None,
+        buttons=None,
+    ):
+        attachment = {
+            "type": "template",
+            "payload": {
+                "template_type": "generic",
+                "elements": [{"title": title}],
+            },
+        }
+        if subtitle is not None:
+            attachment["payload"]["elements"][0]["subtitle"] = subtitle
+        if image_url is not None:
+            attachment["payload"]["elements"][0]["image_url"] = image_url
+        if default_action is not None:
+            attachment["payload"]["elements"][0]["default_action"] = default_action
+        if buttons is not None:
+            attachment["payload"]["elements"][0]["buttons"] = buttons
+        return attachment
+
+    def send_template(self, recipient, template, **kwargs):
+        payload = {
+            "recipient": {"id": recipient},
+            "notification_type": kwargs.get("notification_type", "REGULAR"),
+            "message": {"attachment": {"type": "template", "payload": template}},
+        }
+
+        response = requests.post(
+            API, headers=self.headers, json=payload, params=self.params
+        )
+        return response.status_code == 200
+
+    def send_text(self, recipient, text, notification_type="REGULAR"):
+        total_payload = {
+            "recipient": {"id": recipient},
+            "notification_type": notification_type,
+            "message": {"text": text},
+            "messaging_type": "RESPONSE",
+        }
+
+        response = requests.post(
+            API, headers=self.headers, json=total_payload, params=self.params
+        )
+        return response.status_code == 200
+
+    def send_attachment(self, recipient, attachment, notification_type="REGULAR"):
+        payload = {
+            "recipient": {"id": recipient},
+            "notification_type": notification_type,
+            "message": {"attachment": attachment},
+        }
+
+        response = requests.post(
+            API, headers=self.headers, json=payload, params=self.params
+        )
+        return response.status_code == 200
+
+
+if __name__ == "__main__":
+    api = GraphAPI(os.environ["FB_GRAPH_API_TOKEN"])
+    notification_type = "NO_PUSH"
+
+    api.send_text(
+        recipient=os.environ["FB_TEST_RECIPIENT"],
+        text=os.environ["FB_TEST_TEXT"],
+        notification_type=notification_type,
+    )
+
+    button = api.create_url_button(
+        title=os.environ["FB_TEST_BTN_TITLE"], url=os.environ["FB_TEST_BTN_URL"]
+    )
+    action = api.create_default_action(button)
+    attachment = api.create_single_generic(
+        title=os.environ["FB_TEST_GEN_TITLE"],
+        subtitle=os.environ["FB_TEST_GEN_SUBTITLE"],
+        image_url=os.environ["FB_TEST_GEN_IMAGE_URL"],
+        default_action=action,
+        buttons=[button],
+    )
+    api.send_attachment(
+        recipient=os.environ["FB_TEST_RECIPIENT"],
+        attachment=attachment,
+        notification_type=notification_type,
+    )
---
+++ jellyfin.py
@@ -0,0 +1,87 @@
+from yarl import URL
+import requests
+import json
+import os
+import sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class JellyfinQuickConnectAuth:
+
+    server_url: URL
+    device_id: str
+
+    @property
+    def headers(self):
+        return {
+            "Content-Type": "application/json",
+            "Authorization": f"MediaBrowser Client=\"{self.client}\", Device=Android, DeviceId={self.device_id}, Version=0.0.1"
+        }
+
+    def __init__(self, server_url: str, device_id = "0123456", client="TheBot"):
+        self.server_url = URL(server_url)
+        self.device_id = device_id
+        self.client = client
+
+    def check(self):
+        url = str(self.server_url / "QuickConnect/Enabled")
+        response = requests.get(url)
+        return response.status_code == 200 and response.text == "true"
+
+    def connect(self, secret: str):
+        url = str(self.server_url / "QuickConnect/Connect")
+        response = requests.get(url, params= {
+            "secret": secret,
+        })
+        return response.status_code == 200
+
+    def initiate(self) -> tuple[str, str]:
+        url = str(self.server_url / "QuickConnect/Initiate")
+        response = requests.post(url, headers=self.headers)
+        if response.status_code != 200:
+            raise RuntimeError('Quick connect is not active on this server.')
+        response_json = response.json()
+        return response_json['Secret'], response_json['Code']
+
+    def authenticate(self, secret: str) -> tuple[str, dict]:
+        url = str(self.server_url / "Users/AuthenticateWithQuickConnect")
+        response = requests.post(url, json={
+            "Secret": secret,
+        }, headers=self.headers)
+        if response.status_code != 200:
+            raise RuntimeError('Missing token.')
+        response_json = response.json()
+        return response_json['AccessToken'], response_json['User']
+
+
+# QuickConnect verification script
+if __name__ == "__main__":
+    jf_qcauth = JellyfinQuickConnectAuth(os.getenv('JELLYFIN_SERVER_URL'))
+
+    qc_enabled = jf_qcauth.check()
+    print(f"Jellyfin QuickConnect is{'' if qc_enabled else ' not'} enabled")
+
+    if not qc_enabled:
+        sys.exit(1)
+
+    print("Intiated QuickConnect session:")
+    secret, code = jf_qcauth.initiate()
+    print(f"Secret={"*"*len(secret)}")
+
+    jf_web_url = str(jf_qcauth.server_url) + "/web/#/quickconnect"
+    print(f"Enter QuickConnect code into Jellyfin in app or at {jf_web_url}: {code}")
+
+    print("Press enter when done:", end='')
+    input("")
+
+    qc_success = jf_qcauth.connect(secret)
+    if not qc_success:
+        print('QuickConnect failure!')
+        sys.exit(1)
+    print('QuickConnect success!')
+
+    print('Logging in...')
+    access_token, user = jf_qcauth.authenticate(secret)
+    print(f"AccessToken={"*"*len(access_token)}")
+    print(f"User={json.dumps(user, indent=4)}")
